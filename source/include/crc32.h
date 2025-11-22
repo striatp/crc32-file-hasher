28afd0e34bbcd5abc50e7e9a8cf2ea8863ad6c75
Striatp <sebastien.works@outlook.com>
2025-11-22 13:38:27 +0100

    feat: add crc32 header file ✨

--- source/include/crc32.h
+++ source/include/crc32.h
@@ -1,5 +1,19 @@
 #ifndef CRC32_H
 #define CRC32_H
-#pragma once
 
-#endif
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+uint32_t crc32(const uint8_t* data, size_t len, uint32_t crc);
+
+extern uint32_t crc32_step(uint32_t crc, uint8_t byte);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // CRC32_H
