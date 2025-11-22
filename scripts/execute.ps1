45d35edab8ae0ed16faad1087461cce01bcfca19
Striatp <sebastien.works@outlook.com>
2025-11-22 09:42:06 +0100

    chore: add a powershell script to execute the compiled source code ♻️

---
+++ scripts/execute.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh
+
+param ()
+
+if (-not (Test-Path -Path (Join-Path $PWD build\output.exe))) {
+    Write-Host "[ERROR] " -NoNewline -ForegroundColor Red ;
+    Write-Host "There is no compiled program to execute." ;
+    exit 1 ;
+}
+
+function Execute {
+    [CmdletBinding()]
+    [OutputType([System.Void])]
+    param ()
+
+    & (Join-Path $PWD build\output.exe) ;
+}
+
+Execute ;
